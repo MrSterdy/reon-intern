9fddc8cdcb4efffa0aacaeb621e176c0cb8b9bf5
MrSterdy <mrsterdy@gmail.com>
2026-06-19 19:35:18 +0300

    feat: webhook service

--- src/modules/webhook/webhook.module.ts
+++ src/modules/webhook/webhook.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ApiModule } from '../api/api.module';
+import { WebhookService } from './webhook.service';
 
 @Module({
-    imports: [],
-    providers: [],
-    exports: [],
+    imports: [ApiModule],
+    providers: [WebhookService],
+    exports: [WebhookService],
 })
 export class WebhookModule {}
